8a01846152c486ce6d31da9ce4210b3eb9128ee6
mathurinm <mathurin.massias@gmail.com>
2021-06-21 16:16:13 +0200

    simpler bench script

--- andersoncd/tests/bench_solvers.py
+++ andersoncd/tests/bench_solvers.py
@@ -1,27 +1,19 @@
 import time
 import numpy as np
-from numpy.linalg import norm
 
-from andersoncd.weighted_lasso import celer_primal
+from andersoncd import WeightedLasso
 from celer.datasets import make_correlated_data
 
 X, y, _ = make_correlated_data(
     n_samples=1000, n_features=5_000, snr=5, corr=0.7, random_state=1,
     density=0.2)
-n_samples, n_features = X.shape
-alpha_max = norm(X.T @ y, ord=np.inf) / n_samples
-alpha = 0.01 * alpha_max
 
-
-w = np.zeros(n_features)
-R = y - X @ w
-norms_X_col = norm(X, axis=0)
-weights = np.ones(n_features)
 
 t_start = time.time()
-celer_primal(
-    X, y, alpha, w, R, norms_X_col, weights, max_iter=10,
-    max_epochs=1_000, p0=10, use_acc=True, K=5, verbose=True,
-    tol=1e-10)
-t_ellapsed = time.time() - t_start
-print(t_ellapsed)
+weights = np.random.rand(X.shape[1])a
+weights[:50] = 0
+alpha_max = np.max(np.abs(X[:, 50:].T @ y)) / len(y)
+clf = WeightedLasso(weights=weights, alpha=alpha_max/50, verbose=2)
+clf.fit(X, y)
+t_elapsed = time.time() - t_start
+print(f'Time taken: {t_elapsed:.3s}')
